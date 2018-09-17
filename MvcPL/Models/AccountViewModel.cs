2065ce86dc37ee9ae2da16154a9484c2f39a3585
AntonDr <trollover939@gmail.com>
2018-09-17 18:08:57 +0300

    add some validation

--- MvcPL/Models/AccountViewModel.cs
+++ MvcPL/Models/AccountViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -10,13 +11,29 @@ namespace MvcPL.Models
 {
     public class AccountViewModel
     {
+
         public string AccountID { get; set; }
+
         public decimal Balance { get; set; }
+
+        [Required]
         public TypeOfBankScore AccountType { get; set; }
         public Status AccountStatus { get; set; }
         public int BonsuPoint { get; set; }
+
+        [Required]
+        [DisplayName("First name")]
+        [StringLength(15,MinimumLength = 2)]
         public string HolderFirstName { get; set; }
+
+        [Required]
+        [DisplayName("Last name")]
+        [StringLength(20,MinimumLength = 2)]
         public string HolderLastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [DisplayName("Email")]
         public string HolderEmail { get; set; }
     }
 }
